a4944907efebb15e866fa3b214ebbd3fcf2e30ce
Galo Sandoval <galosan92@gmail.com>
2026-01-10 19:31:45 -0600

    deploy2

--- internal/handlers/health.go
+++ internal/handlers/health.go
@@ -14,7 +14,7 @@ type HealthHandler struct {
 }
 
 func NewHealthHandler(db *pgxpool.Pool) *HealthHandler {
-	fmt.Println("NewHealthHandlerv2")
+	fmt.Println("NewHealthHandlerv3")
 
 	return &HealthHandler{db: db}
 }
